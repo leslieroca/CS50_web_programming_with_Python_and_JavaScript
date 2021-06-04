a81841e26238aac6f85a82e152204e147ada6113
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-06-04 19:13:50 -0400

    Python input, print and conditions

---
+++ lecture2_Python/name.py
@@ -0,0 +1,13 @@
+name = input("Name: ")
+print("Hello, " + name + "!")
+print(f"Hello, {name}, nice to meet you!")
+
+#CONDITIONS
+n = int(input("Numer: "))
+
+if n > 0:
+    print(f"{n} is positive")
+elif n == 0:
+    print(f"{n} is 0")
+else: 
+    print(f"{n} is negative")

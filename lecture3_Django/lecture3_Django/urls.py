85907036a2c2ac674c513deda819d7b4dbc0cde6
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-07-16 21:31:02 -0400

    Add new url

--- lecture3_Django/lecture3_Django/urls.py
+++ lecture3_Django/lecture3_Django/urls.py
@@ -18,5 +18,5 @@ from django.urls import include path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello', include("hello.urls"))
+    path('hello/', include("hello.urls"))
 ]

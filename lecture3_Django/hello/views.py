6244c96df31ffd088182a78b341fe403a3d3ed00
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-07-26 19:24:46 -0400

    Create templates and render them to the page

--- lecture3_Django/hello/views.py
+++ lecture3_Django/hello/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!") 
+    return render(request, "hello/index.html") 
 
 def greet(request, name):
     return HttpResponse(f"Hello, {name.capitalize()}!")

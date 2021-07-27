cc382497970165198426ceaaea2712c8531fedf5
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-07-26 20:37:09 -0400

    Style the page, creates a css file and link it to the html

--- lecture3_Django/newyear/views.py
+++ lecture3_Django/newyear/views.py
@@ -7,5 +7,5 @@ from django.shortcuts import render
 def index(request):
     now = datetime.datetime.now()
     return render(request, "newyear/index.html", {
-        "newyear": now.month == 1 and now.day == 1
+        "newyear": True
     })

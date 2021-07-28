e4eadc1c63be596fd739bc1673f37f8df723a2fb
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-07-27 22:07:20 -0400

    Create the Add view

--- lecture3_Django/tasks/views.py
+++ lecture3_Django/tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+tasks = ["foo", "bar", "baz"]
+
 # Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+def add(request):
+    return render(request, "tasks/add.html")

3d472fe0d33b82b385187f37c1670f65619b0776
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-07-29 14:18:57 -0400

    Create forms using Django instead of raw HTML

--- lecture3_Django/tasks/views.py
+++ lecture3_Django/tasks/views.py
@@ -1,6 +1,12 @@
+from django import forms
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+
 
 # Create your views here.
 def index(request):
@@ -9,4 +15,6 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })

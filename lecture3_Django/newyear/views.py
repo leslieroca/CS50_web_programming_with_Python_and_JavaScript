63c7ffa001494c7837b19dfa9b0675b550d546e2
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-07-31 23:00:00 -0400

    Render correct value of newyear depending of the datetime

--- lecture3_Django/newyear/views.py
+++ lecture3_Django/newyear/views.py
@@ -7,5 +7,5 @@ from django.shortcuts import render
 def index(request):
     now = datetime.datetime.now()
     return render(request, "newyear/index.html", {
-        "newyear": True
+        "newyear": now.month == 1 and now.day == 1
     })

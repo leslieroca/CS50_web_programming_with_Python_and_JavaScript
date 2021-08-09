ba1fe4ab685d263f1f6d83dbb5c5794e69c039d7
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-08-09 17:11:42 -0400

    Remove Proprity form

--- lecture3_Django/tasks/views.py
+++ lecture3_Django/tasks/views.py
@@ -6,7 +6,6 @@ tasks = ["foo", "bar", "baz"]
 # Create froms using Django way
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 
 # Create your views here.
@@ -25,7 +24,7 @@ def add(request):
             return render(request, "tasks/index.html", {
                 "form": form
             })
-            
+
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })

d64976e88b22931c39a41b26a3757ccccee25329
Leslie Rodriguez <leslie.roca56@gmail.com>
2021-08-09 18:00:20 -0400

    Fix typo 'secction' to 'session'

--- lecture3_Django/tasks/views.py
+++ lecture3_Django/tasks/views.py
@@ -8,14 +8,13 @@ from django.urls import reverse
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
-
 # Create your views here.
 def index(request):
-    if "tasks" not in request.secction:
-        request.secction["tasks"] = []
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
          
     return render(request, "tasks/index.html", {
-        "tasks": request.secction["tasks"]
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
